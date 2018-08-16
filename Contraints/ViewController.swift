533ce8684da01fd3dd8978dcebd5b05bc8e3daa6
Maestro <Maestro@imac-de-maestro-4.local>
2018-08-16 13:04:42 -0700

    Initial Commit

---
+++ Contraints/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Contraints
+//
+//  Created by Maestro on 16/08/18.
+//  Copyright © 2018 ULSA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
